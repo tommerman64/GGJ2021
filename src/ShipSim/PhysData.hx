8bb05284fc4065c504470e5e43a94f3ac3328f2b
Tom C <tcontilfc8@gmail.com>
2021-01-28 19:18:14 -0800

    phys structures

---
+++ src/ShipSim/PhysData.hx
@@ -0,0 +1,16 @@
+package shipSim;
+
+import h3d.Vector;
+import h2d.col.Circle;
+
+class ColliderData
+{
+    public var collider:Circle;
+}
+
+class ShipMovement
+{
+    public var velocity: Vector;
+    public var rotation: Float;
+    public var rotationalVelocity: Float;
+}
